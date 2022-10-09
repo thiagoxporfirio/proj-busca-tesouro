6233e6bfa6630866d922423fd98afc3bff927c35
Thiago Porfirio <thiagovporfirio@gmail.com>
2022-10-09 12:54:02 -0300

    botoes e links funcionando na aba planos

--- assinatura/appassinatura.js
+++ assinatura/appassinatura.js
@@ -5,13 +5,28 @@ let identidadeUser = JSON.parse(localStorage.getItem('_DadosUser2') || '{}')
 
 
 let btnUserid = document.querySelector('.userid')
-
-
+let btnUserid2 = document.querySelector('.userid2')
+let btnUserid3 = document.querySelector('.userid3')
 
 
 
 if(btnUserid){
     btnUserid.addEventListener('click', () => {
         window.location.href = `https://calm-caverns-77349.herokuapp.com/?userID=${usuarioid}`
+    })
+}
+
+
+if(btnUserid2){
+    btnUserid2.addEventListener('click', () => {
+        window.location.href = `https://calm-caverns-77349.herokuapp.com/cliente2/?userID=${usuarioid}`
+        console.log("clicou")
+    })
+}
+
+
+if(btnUserid3){
+    btnUserid3.addEventListener('click', () => {
+        window.location.href = `https://calm-caverns-77349.herokuapp.com/cliente3/?userID=${usuarioid}`
     })
 }
