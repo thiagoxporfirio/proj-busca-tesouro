39936069bab0b2ceefbd08c8cdbbc7c4896c1383
Thiago Porfirio <thiagovporfirio@gmail.com>
2022-10-22 13:56:11 -0300

    adicionado span para fazlogin caso for assinar um plano

--- assinatura/appassinatura.js
+++ assinatura/appassinatura.js
@@ -1,15 +1,20 @@
    
-let usuarioid = identidadeUser.dados.UserId
+let usuarioid = identidadeUser?.dados?.UserId
 console.log(usuarioid)
 
 let btnUserid = document.querySelector('.userid')
 let btnUserid2 = document.querySelector('.userid2')
 let btnUserid3 = document.querySelector('.userid3')
 
+let spanfazlogin = document.querySelector('.container-error-validate')
 
 
 if(btnUserid){
     btnUserid.addEventListener('click', () => {
+        if(typeof usuarioid == "undefined"){
+            spanfazlogin.style.display = "flex"
+            return
+        }
         window.location.href = `https://calm-caverns-77349.herokuapp.com/?userID=${usuarioid}`
     })
 }
@@ -17,6 +22,10 @@ if(btnUserid){
 
 if(btnUserid2){
     btnUserid2.addEventListener('click', () => {
+        if(typeof usuarioid == "undefined"){
+            spanfazlogin.style.display = "flex"
+            return
+        }
         window.location.href = `https://calm-caverns-77349.herokuapp.com/cliente2/?userID=${usuarioid}`
         console.log("clicou")
     })
@@ -25,6 +34,10 @@ if(btnUserid2){
 
 if(btnUserid3){
     btnUserid3.addEventListener('click', () => {
+        if(typeof usuarioid == "undefined"){
+            spanfazlogin.style.display = "flex"
+            return
+        }
         window.location.href = `https://calm-caverns-77349.herokuapp.com/cliente3/?userID=${usuarioid}`
     })
 }
